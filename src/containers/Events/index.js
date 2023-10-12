767aecae833a8bcea5ea5a524c7b25db8b8796aa
Mathias <mthsvilly@gmail.com>
2023-10-12 15:14:46 +0200

    filtre realisation

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,10 +13,10 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
+  const filteredEvents = (    
     (!type
       ? data?.events
-      : data?.events) || []
+      : data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
