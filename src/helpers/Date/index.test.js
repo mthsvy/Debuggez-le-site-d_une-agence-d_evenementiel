a50ec8226de245f82dc0e77f5d94022a0f10434a
Mathias <mthsvilly@gmail.com>
2023-10-28 10:44:49 +0200

    Test helper

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,15 @@
-/**
- * 
- */
+import { getMonth } from ".";
 
 describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      expect(getMonth(new Date("2022-01-01"))).toEqual("janvier");
+      // to implement
     });
-})
 
+    it("the function return juillet for 2022-07-08 as date", () => {
+      expect(getMonth(new Date("2022-07-08"))).toEqual("juillet");
+      // to implement
+    });
+  });
+});
