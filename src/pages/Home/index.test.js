2e7ba94e089fb190a397ef8a74e04fdc0689e3e2
Mathias <mthsvilly@gmail.com>
2023-10-28 10:32:17 +0200

    Test de la page Page Home

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,5 +1,42 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
+import { api, DataProvider } from "../../contexts/DataContext";
+
+const data = {
+  events: [
+    {
+      id: 1,
+      type: "soirée entreprise",
+      date: "2022-04-29T20:28:45.744Z",
+      title: "Conférence #productCON",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
+      description:
+        "Présentation des outils analytics aux professionnels du secteur",
+      nb_guesses: 1300,
+      periode: "24-25-26 Février",
+      prestations: [
+        "1 espace d’exposition",
+        "1 scéne principale",
+        "2 espaces de restaurations",
+        "1 site web dédié",
+      ],
+    },
+
+    {
+      id: 2,
+      type: "forum",
+      date: "2022-05-29T20:28:45.744Z",
+      title: "Forum #productCON",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
+      description:
+        "Présentation des outils analytics aux professionnels du secteur",
+      nb_guesses: 1300,
+      periode: "24-25-26 Février",
+      prestations: ["1 espace d’exposition", "1 scéne principale"],
+    },
+  ],
+  focus: [],
+};
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -27,16 +64,35 @@ describe("When Form is created", () => {
 });
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
+  it("a list of event card is displayed", async () => {
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+    );
+    await screen.findByText("avril");
     // to implement
   });
-  it("a list a people is displayed", () => {
+  it("a list a people is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Samira");
     // to implement
   });
-  it("a footer is displayed", () => {
+  it("a footer is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Notre derniére prestation");
     // to implement
   });
-  it("an event card, with the last event, is displayed", () => {
+
+  it("an event card, with the last event, is displayed", async () => {
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+    );
+    await screen.findByText("boom");
     // to implement
   });
 });
